24116ece0bf6aa2289ef9b1129885b447604a011
Dmitry Raidman <eaglei15@gmail.com>
2018-10-10 00:27:04 +0200

    Making some changes for build

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { Credentials } from './lib/credentials'
+import { LoggingService } from './lib/loggingservice'
+import { EventService } from './lib/eventservice'
+export { Credentials, LoggingService, EventService }
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { Credentials } from './credentials'
-import { LoggingService } from './loggingservice'
-import { EventService } from './eventservice'
-export { Credentials, LoggingService, EventService }
