c46a2bb258c409191ef29a2b8bfb79917c6cbdd3
Xavier Homs <xhoms@paloaltonetworks.com>
2019-03-13 16:11:28 +0100

    hotfix

--- example/credential_fs.ts
+++ example/credential_fs.ts
@@ -2,7 +2,7 @@ import { fsCredentialsFactory } from 'pancloud-nodejs'
 
 export async function main(): Promise<void> {
     let credProv = await fsCredentialsFactory({ secret: 'mysecret' })
-    let c = await credProv.registerManualDatalake('hello', 'fJv1DfHQsp3jpISTg6T0e5xyOWtLCaNdWnHyMBlWGl')
+    let c = await credProv.registerManualDatalake('hello', '<datalake-refresh-token>')
     let d = new Date(await c.getExpiration() * 1000)
     console.log(`Access Token: ${await c.getAccessToken()}\nValid until: ${d.toISOString()}`)
 }
